5525901c0bead707deca7e31184bb9717daa6dd2
Felipe Alexandre <felipeas314@gmail.com>
2024-07-27 13:57:08 -0300

    refactor: add constructor in class Product and change variable name to private

--- src/main/java/br/com/labs/Product.java
+++ src/main/java/br/com/labs/Product.java
@@ -2,9 +2,22 @@ package br.com.labs;
 
 public class Product {
 
-    public String name;
+    private String name;
 
     private float price;
+
+    public Product(String name,float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void setPrice(float price) {
         this.price = price;
